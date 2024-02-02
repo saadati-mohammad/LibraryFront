690b78594d11bf733a1da11fe25e7e9aef5db522
library <mohammadsaadati2003@gmail.com>
2024-02-02 22:10:55 +0330

    test for Commit

---
+++ src/app/change-cmp/change-cmp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-cmp',
+  standalone: true,
+  imports: [],
+  templateUrl: './change-cmp.component.html',
+  styleUrl: './change-cmp.component.css'
+})
+export class ChangeCmpComponent {
+asdfsdfffffff: string = '';
+}
